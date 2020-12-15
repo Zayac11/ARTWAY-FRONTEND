81320b367b65b744db1eaa28ef6576186d54dc5a
Zayac11 <podolskiy.2001@mail.ru>
2020-12-15 17:16:03 +0300

    added common container with button

---
+++ src/Common/BackBtn/BackBtn.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import s from './BackBtn.module.css'
+import prev from "../../assets/images/next.svg";
+
+const BackBtn = (props) => {
+    return (
+        <button onClick={() => props.history.goBack()} className={s.backBtn}>
+            <img src={prev} alt="back"/>
+        </button>
+    );
+}
+
+export default BackBtn;
