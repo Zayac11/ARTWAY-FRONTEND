4425583f1f961443fcfc09e0dd2dc5686ecc4539
Zayac11 <brunogrbstudios@gmail.com>
2021-01-19 13:35:52 +0300

    added hoc with common create worker logic

--- src/Components/Container/CreateWorker/CreateWorker.jsx
+++ src/Components/Container/CreateWorker/CreateWorker.jsx
@@ -4,97 +4,26 @@ import {Redirect} from "react-router-dom";
 import s from './CreateWorker.module.css'
 import Input from "../../../Common/Input/Input";
 import {createWorker} from "../../../redux/admin-reducer";
+import {compose} from "redux";
+import {CommonCreateWorkerLogic} from "../../../hoc/CommonCreateWorkerLogic";
 
 class CreateWorker extends React.Component {
 
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            last_name: "",
-            first_name: "",
-            middle_name: "",
-            email: "",
-            password: "",
-            confirm_password: "",
-            role: "museum_admins",
-
-            isEmptyInputs: false, //Все ли поля пустые
-            isEmailWrong: false, //Если почтовый адрес направильный
-            isPasswordMatch: true, //Если пароли совпадают
-            isCreate: false, //Если успешно создано
-        }
-
-        this.handleFindKey = this.handleFindKey.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
-        this.handleChangeInputs = this.handleChangeInputs.bind(this)
-        this.handleChange = this.handleChange.bind(this)
-        this.handleChangeRadio = this.handleChangeRadio.bind(this)
+    createWorker() {
+        this.props.createWorker(
+            this.props.last_name,
+            this.props.first_name,
+            this.props.middle_name,
+            this.props.email,
+            this.props.password,
+            this.props.role)
     }
 
-    handleChange(e) {
-        let name = e.target.name
-        let value = e.target.value
-
-        this.setState({
-            [name]: value,
-        })
-    }
-    handleChangeRadio(e) {
-        let value = e.target.value
-
-        this.setState({
-            'role': value,
-        })
-    }
-
-    handleFindKey(e) {
-        if(e.keyCode === 13) { // Логин с нажатием enter
-            this.handleSubmit()
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if(prevProps.isRight !== this.props.isRight) {
+            this.createWorker()
         }
     }
-
-    handleSubmit() {
-        let mail = /^[\w-\d*]+@[\w\d]+(\.\w{2,4})$/
-        if(this.state.first_name === '' || this.state.last_name === '' || this.state.email === '' || this.state.password === '') {
-            this.setState({
-                isEmptyInputs: true
-            })
-        }
-        else if(this.state.password !== this.state.confirm_password) {
-            this.setState({
-                isPasswordMatch: false,
-            })
-        }
-        else if(!mail.test(this.state.email)) {
-            this.setState({
-                isEmailWrong: true,
-            })
-        }
-        else {
-            this.props.createWorker(
-                this.state.last_name,
-                this.state.first_name,
-                this.state.middle_name,
-                this.state.email,
-                this.state.password,
-                this.state.role)
-            this.setState({
-                isCreate: true,
-            })
-        }
-
-        // console.log(this.state)
-    }
-
-    handleChangeInputs() {
-        this.setState({
-            isEmptyInputs: false,
-            isEmailWrong: false,
-            isPasswordMatch: true,
-        })
-    }
-
 
     componentDidMount() {
 
@@ -105,48 +34,47 @@ class CreateWorker extends React.Component {
         if(!this.props.is_museum_super_adminTest) {
             return <Redirect to={'/'} />
         }
-        if(this.state.isCreate) {
+        if(this.props.isCreate) {
             return <Redirect to={'/m-admin/hr-management'} />
         }
-
         return (
             <div className={s.container}>
                 <h1>Создание пользователя музея</h1>
-                <Input text={'Фамилия'} type={'text'} name={'last_name'} handleFocus={this.handleChangeInputs} handleFindKey={this.handleFindKey} handleChange={this.handleChange} value={this.state.last_name} />
-                <Input text={'Имя'} type={'text'} name={'first_name'} handleFocus={this.handleChangeInputs} handleFindKey={this.handleFindKey} handleChange={this.handleChange} value={this.state.first_name} />
-                <Input text={'Отчество'} type={'text'} name={'middle_name'} handleFocus={this.handleChangeInputs} handleFindKey={this.handleFindKey} handleChange={this.handleChange} value={this.state.middle_name} />
-                <Input text={'Email'} type={'text'} name={'email'} handleFocus={this.handleChangeInputs} handleFindKey={this.handleFindKey} handleChange={this.handleChange} value={this.state.email} />
-                <Input text={'Пароль'} type={'password'} name={'password'} handleFocus={this.handleChangeInputs} handleFindKey={this.handleFindKey} handleChange={this.handleChange} value={this.state.password} />
-                <Input text={'Повторите пароль'} type={'password'} name={'confirm_password'} handleFocus={this.handleChangeInputs} handleFindKey={this.handleFindKey} handleChange={this.handleChange} value={this.state.confirm_password} />
+                <Input text={'Фамилия'} type={'text'} name={'last_name'} handleFocus={this.props.handleChangeInputs} handleFindKey={this.props.handleFindKey} handleChange={this.props.handleChange} value={this.props.last_name} />
+                <Input text={'Имя'} type={'text'} name={'first_name'} handleFocus={this.props.handleChangeInputs} handleFindKey={this.props.handleFindKey} handleChange={this.props.handleChange} value={this.props.first_name} />
+                <Input text={'Отчество'} type={'text'} name={'middle_name'} handleFocus={this.props.handleChangeInputs} handleFindKey={this.props.handleFindKey} handleChange={this.props.handleChange} value={this.props.middle_name} />
+                <Input text={'Email'} type={'text'} name={'email'} handleFocus={this.props.handleChangeInputs} handleFindKey={this.props.handleFindKey} handleChange={this.props.handleChange} value={this.props.email} />
+                <Input text={'Пароль'} type={'password'} name={'password'} handleFocus={this.props.handleChangeInputs} handleFindKey={this.props.handleFindKey} handleChange={this.props.handleChange} value={this.props.password} />
+                <Input text={'Повторите пароль'} type={'password'} name={'confirm_password'} handleFocus={this.props.handleChangeInputs} handleFindKey={this.props.handleFindKey} handleChange={this.props.handleChange} value={this.props.confirm_password} />
 
                 <div>
                     <div>
                         Роль работника
                     </div>
-                    <div><input name={'role'} type="radio" checked={this.state.role === 'museum_admins'} value='museum_admins' onChange={this.handleChangeRadio}/>Администратор музея</div>
-                    <div><input name={'role'} type="radio" checked={this.state.role === 'museum_cashiers'} value='museum_cashiers' onChange={this.handleChangeRadio}/>Кассир</div>
+                    <div><input name={'role'} type="radio" checked={this.props.role === 'museum_admins'} value='museum_admins' onChange={this.props.handleChangeRadio}/>Администратор музея</div>
+                    <div><input name={'role'} type="radio" checked={this.props.role === 'museum_cashiers'} value='museum_cashiers' onChange={this.props.handleChangeRadio}/>Кассир</div>
                 </div>
 
                 {
-                    this.state.isEmptyInputs &&
+                    this.props.isEmptyInputs &&
                         <div className={'form__wrong'}>
                             Заполните все поля, кроме отчества
                         </div>
                 }
                 {
-                    !this.state.isPasswordMatch &&
+                    !this.props.isPasswordMatch &&
                         <div className={'form__wrong'}>
                             Пароли не совпадают
                         </div>
                 }
                 {
-                    this.state.isEmailWrong &&
+                    this.props.isEmailWrong &&
                         <div className={'form__wrong'}>
                             Пожалуйста, введите корректный email
                         </div>
                 }
 
-                <button onClick={this.handleSubmit}>Создать</button>
+                <button onClick={this.props.handleSubmit}>Создать</button>
 
             </div>
         );
@@ -161,4 +89,9 @@ let mapStateToProps = (state) => {
     }
 }
 
-export default connect(mapStateToProps,{createWorker})(CreateWorker);
+export default compose(
+    connect(mapStateToProps, {createWorker}),
+    CommonCreateWorkerLogic,
+)(CreateWorker)
+
+// export default connect(mapStateToProps,{createWorker})(CreateWorker);
---
+++ src/hoc/CommonCreateWorkerLogic.js
@@ -0,0 +1,124 @@
+import React from "react";
+import {connect} from "react-redux";
+
+let mapStateToPropsForRedirect = (state) => {
+    return {
+        isUserServiceAdmin: state.service.isUserServiceAdmin,
+    };
+}
+
+export const CommonCreateWorkerLogic = (Component) => {
+
+    class CommonCreateWorkerLogic extends React.Component {
+
+        constructor(props) {
+            super(props);
+
+            this.state = {
+                last_name: "",
+                first_name: "",
+                middle_name: "",
+                email: "",
+                password: "",
+                confirm_password: "",
+                role: "museum_admins",
+
+                isEmptyInputs: false, //Все ли поля пустые
+                isEmailWrong: false, //Если почтовый адрес направильный
+                isPasswordMatch: true, //Если пароли совпадают
+                isCreate: false, //Если успешно создано
+                isRight: false, //Если все поля успешно заполнены
+            }
+
+            this.handleFindKey = this.handleFindKey.bind(this)
+            this.handleSubmit = this.handleSubmit.bind(this)
+            this.handleChangeInputs = this.handleChangeInputs.bind(this)
+            this.handleChange = this.handleChange.bind(this)
+            this.handleChangeRadio = this.handleChangeRadio.bind(this)
+        }
+
+        handleChange(e) {
+            let name = e.target.name
+            let value = e.target.value
+
+            this.setState({
+                [name]: value,
+            })
+        }
+
+        handleChangeRadio(e) {
+            let value = e.target.value
+
+            this.setState({
+                'role': value,
+            })
+        }
+
+        handleFindKey(e) {
+            if (e.keyCode === 13) { // Логин с нажатием enter
+                this.handleSubmit()
+            }
+        }
+
+        handleSubmit() {
+            debugger
+            let mail = /^[\w-\d*]+@[\w\d]+(\.\w{2,4})$/
+            if (this.state.first_name === '' || this.state.last_name === '' || this.state.email === '' || this.state.password === '') {
+                this.setState({
+                    isEmptyInputs: true
+                })
+            } else if (this.state.password !== this.state.confirm_password) {
+                this.setState({
+                    isPasswordMatch: false,
+                })
+            } else if (!mail.test(this.state.email)) {
+                this.setState({
+                    isEmailWrong: true,
+                })
+            } else {
+                this.setState({
+                    isCreate: true,
+                    isRight: true,
+                })
+            }
+        }
+
+        handleChangeInputs() {
+            this.setState({
+                isEmptyInputs: false,
+                isEmailWrong: false,
+                isPasswordMatch: true,
+            })
+        }
+
+        render() {
+
+            return (
+                <Component {...this.props}
+                           isCreate={this.state.isCreate}
+                           isRight={this.state.isRight}
+                           isEmptyInputs={this.state.isEmptyInputs}
+                           isEmailWrong={this.state.isEmailWrong}
+                           isPasswordMatch={this.state.isPasswordMatch}
+                           last_name={this.state.last_name}
+                           middle_name={this.state.middle_name}
+                           first_name={this.state.first_name}
+                           email={this.state.email}
+                           password={this.state.password}
+                           role={this.state.role}
+                           handleSubmit={this.handleSubmit}
+                           handleChange={this.handleChange}
+                           handleChangeInputs={this.handleChangeInputs}
+                           handleFindKey={this.handleFindKey}
+                           handleChangeRadio={this.handleChangeRadio}
+                           changeCreate={this.changeCreate}
+                />
+            );
+        }
+
+    }
+
+    let ConnectedCommonCreateWorkerComponent = connect(mapStateToPropsForRedirect)(CommonCreateWorkerLogic);
+
+    return ConnectedCommonCreateWorkerComponent;
+}
