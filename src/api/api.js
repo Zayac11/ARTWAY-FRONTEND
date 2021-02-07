524e189494697508d26cdfb7f447611eb13d71f2
Zayac11 <brunogrbstudios@gmail.com>
2021-02-07 20:56:37 +0300

    changed api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import * as axios from "axios";
 
-const debug = true
+const debug = false
 //backend
 //true for localhost
 //false for prod.
@@ -48,14 +48,6 @@ export const artifactApi = {
         return axios.get(baseUrl + `api/artifacts/${id}`)
             .then(response => response)
     },
-    getArtifactsList() { //Получение списка экспонатов
-        return axios.get(baseUrl + `api/artifacts`)
-            .then(response => response)
-    },
-    // getArtifactsQr(id) { //Получение qr экспоната по id
-    //     return axios.get(baseUrl + `api/artifacts/${id}/qr-code`)
-    //         .then(response => response)
-    // }
 }
 
 export const museumApi = {
@@ -213,17 +205,6 @@ export const museumApi = {
 }
 
 export const authApi = {
-    // account() {//Проверка пользователя
-    //     const accessToken = 'Bearer  ' + localStorage.getItem('accessToken')
-    //     let myHeaders = new Headers();
-    //     myHeaders.append("Authorization", accessToken);
-    //     let requestOptions = {
-    //         method: "GET",
-    //         headers: myHeaders,
-    //         redirect: 'follow',
-    //     }
-    //     return fetch(baseUrl + `api/account`, requestOptions)
-    // },
 
     getStatus() {//Проверка пользователя
         let localToken = localStorage.getItem('accessToken')
@@ -241,24 +222,9 @@ export const authApi = {
         return fetch(baseUrl + `api/user_statuses`, requestOptions)
     },
 
-
-
-    accountChange(last_name, first_name, middle_name, phone_number) {//Изменение данных пользователя
-        let options = getOptions([{name: 'last_name', value: last_name}, {name: 'first_name', value: first_name}, {name: 'middle_name', value: middle_name}, {name: 'phone_number', value: phone_number}], true,  'PUT')
-        return fetch(baseUrl + `api/account`, options)
-    },
     login(username, password) {//Логин
         let options = getOptions([{name: 'username', value: username}, {name: 'password', value: password}], false,  'POST')
         return fetch(baseUrl + `auth/jwt/create/`, options)
-    },
-    register(username, password, email, last_name, first_name, middle_name, phone_number) { //Регистрация
-        let options = getOptions([{name: 'username', value: username}, {name: 'password', value: password}, {name: 'email', value: email},
-            {name: 'last_name', value: last_name}, {name: 'first_name', value: first_name}, {name: 'middle_name', value: middle_name}, {name: 'phone_number', value: phone_number}], false,  'POST')
-        return fetch(baseUrl + `auth/users/`, options)
-    },
-    activation(uid, token) { //Активация аккаунта
-        let options = getOptions([{name: 'uid', value: uid}, {name: 'token', value: token}], false,  'POST')
-        return fetch(baseUrl + `auth/users/activation/`, options)
     },
     setPassword(current_password, new_password, re_new_password) { //Смена пароля
         let options = getOptions([{name: 'new_password', value: new_password}, {name: 're_new_password', value: re_new_password}, {name: 'current_password', value: current_password}], true,  'POST')
@@ -267,10 +233,6 @@ export const authApi = {
     resetPassword(email) { //Смена пароля
         let options = getOptions([{name: 'email', value: email}], false,  'POST')
         return fetch(baseUrl + `auth/users/reset_password/`, options)
-    },
-    accountRecovery(email) { //Проверка email пользователя
-        let options = getOptions([{name: 'email', value: email}], false,  'POST')
-        return fetch(baseUrl + `api/account_recovery`, options)
     },
     resetPasswordConfirm(uid, token, new_password, re_new_password) { //Смена пароля
         let options = getOptions([{name: 'uid', value: uid}, {name: 'token', value: token}, {name: 'new_password', value: new_password}, {name: 're_new_password', value: re_new_password}], false,  'POST')
