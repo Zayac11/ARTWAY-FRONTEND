8332b6f4fa4671e27ca3b8e3709a9e53d933f92d
Zayac11 <brunogrbstudios@gmail.com>
2021-02-27 17:55:02 +0300

    ui images fix

--- src/Common/Image/Image.jsx
+++ src/Common/Image/Image.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "react-image-gallery/styles/css/image-gallery.css";
 import ImageGallery from "react-image-gallery";
 import s from './Image.module.css'
+import './Image.css'
 
 const Image = (props) => {
 
@@ -20,6 +21,8 @@ const Image = (props) => {
                     showThumbnails={false}
                     showPlayButton={false}
                     autoPlay={false}
+                    originalClass={s.original}
+                    thumbnailClass ={s.original}
                 />
             </div>
         </div>
